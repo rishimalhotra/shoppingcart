01bf5a04713ce46c296c04ae28f51b8f3facff5d
Rishi Malhotra <rishirmalhotra@gmail.com>
2020-06-02 16:43:59 -0400

    just show price
    just showing price not the other stuff

--- shopping_cart.py
+++ shopping_cart.py
@@ -27,12 +27,13 @@ products = [
 # info capture/ input
 #
 
-selected_id = input("Please input a proudct identifier: ") #string version , "9" string
-matching_product = [p for p in products if str(p["id"]) == str(selected_id)] #p identifies the variable of each item in the list that we have defined as products. return each product dictionary for each prodcut dictionary in our list of products if our product dictoinary id value matches.
+selected_id = input("Please input a product identifier: ") #string version , "9" string
+matching_products = [p for p in products if str(p["id"]) == str(selected_id)] #p identifies the variable of each item in the list that we have defined as products. return each product dictionary for each prodcut dictionary in our list of products if our product dictoinary id value matches.
 #if say 9 then should return product with id of 9
 matching_product = matching_products[0]
-print(matching_product)
-print(type(matching_product))
+#print(matching_product)
+#print(type(matching_product))
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 # def to_usd(my_price):
 #     """
